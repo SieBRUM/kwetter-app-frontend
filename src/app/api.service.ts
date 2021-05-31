d8d87ad55a65c26e7e59b4276e09874cf0a730fc
SieBrum <siebrenkraak@hotmail.com>
2021-05-31 23:15:51 +0200

    Change endpoint based on env

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ILogin } from './models/login.model';
 import { IRegister } from './models/register.model';
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  API_GATEWAY = 'https://localhost:84/api/';
+  API_GATEWAY = environment.url;
 
   constructor(private http: HttpClient) { }
 
@@ -18,6 +19,9 @@ export class ApiService {
     return this.http.get<any>(`${this.API_GATEWAY}kweet/timeline`, { observe: 'response' });
   }
 
+  getTimeline(): Observable<HttpResponse<any>> {
+    return this.http.get<any>(`${this.API_GATEWAY}kweet/timeline`, { observe: 'response' });
+  }
   /* POST calls */
   registerUser(user: IRegister): Observable<HttpResponse<any>> {
     return this.http.post<any>(`${this.API_GATEWAY}user`, user, { observe: 'response' });
@@ -26,4 +30,6 @@ export class ApiService {
   loginUser(user: ILogin): Observable<HttpResponse<string>> {
     return this.http.post<string>(`${this.API_GATEWAY}authentication/login`, user, { observe: 'response' });
   }
+
+
 }
